32495eea2bacf5ada17325ea69401e97f316933f
Shawn Feng <Shawnfeng92@gmail.com>
2019-06-08 09:08:12 -0500

    Update Portfolio.R

--- code/Portfolio.R
+++ code/Portfolio.R
@@ -1,17 +1,17 @@
-install.packages("C:/Users/Shawn/Documents/GitHub/PortfolioAnalytics",
-                 repos = NULL, type = "source")
 library(PortfolioAnalytics)
 library(quadprog)
 library(osqp)
+
+source("~/GitHub/PortfolioAnalytics/R/optimize.portfolio.R")
 
 # Data
 result <- read.csv("~/GitHub/GSoC/data/.combined.csv")
 result <- xts(result[,2:13], order.by = as.Date(as.character(result[,1]), format = "%m/%d/%Y"))
 
-CTAs <- colnames(result)
-GSoC.CTA <- portfolio.spec(assets = CTAs)
+GSoC.CTA <- portfolio.spec(assets = colnames(result))
 
-GSoC.CTA <- add.constraint(portfolio = GSoC.CTA, type = "weight_sum", min_sum = 1, max_sum = 1)
+GSoC.CTA <- add.constraint(portfolio = GSoC.CTA, type = "weight_sum", min_sum = 0.5, max_sum = 1.5)
+GSoC.CTA <- add.constraint(portfolio = GSoC.CTA, type = "long_only")
 GSoC.CTA <- add.constraint(portfolio = GSoC.CTA, type = "return", return_target = 0.007)
 
 GSoC.CTA <- add.objective(GSoC.CTA, type = "return", name = "mean")
