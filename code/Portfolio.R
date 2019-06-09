e97aa4b1e9e0a2df31d7df433508690a9f364b3e
Shawn Feng <Shawnfeng92@gmail.com>
2019-06-09 08:09:09 -0500

    Update Portfolio.R

--- code/Portfolio.R
+++ code/Portfolio.R
@@ -12,11 +12,21 @@ GSoC.CTA <- portfolio.spec(assets = colnames(result))
 
 GSoC.CTA <- add.constraint(portfolio = GSoC.CTA, type = "weight_sum", min_sum = 0.5, max_sum = 1.5)
 GSoC.CTA <- add.constraint(portfolio = GSoC.CTA, type = "long_only")
-GSoC.CTA <- add.constraint(portfolio = GSoC.CTA, type = "return", return_target = 0.007)
+# GSoC.CTA <- add.constraint(portfolio = GSoC.CTA, type = "return", return_target = 0.007)
 
 GSoC.CTA <- add.objective(GSoC.CTA, type = "return", name = "mean")
 GSoC.CTA <- add.objective(GSoC.CTA, type = "risk", name = "StdDev")
 
-final <- optimize.portfolio(R = result, GSoC.CTA, optimize_method = "osqp", verbos = 0)
+methodsList <- c("DEoptim", "random", "pso", "GenSA", "osqp")
 
+test <- function(method) {
+  final <- optimize.portfolio(R = result, GSoC.CTA, optimize_method = method, verbos = 0)
+  w <- final$weights
+  print(method)
+  print(sum(w))
+  print(mean(result %*% w) / sd(result %*% w))
+}
 
+for (i in methodsList) {
+  test(i)
+}
