d20c3057472cf9b11f138b52b2fbf9aa990f9390
Shawn Feng <Shawnfeng92@gmail.com>
2019-06-09 22:13:14 -0500

    Update Portfolio.R

--- code/Portfolio.R
+++ code/Portfolio.R
@@ -12,15 +12,17 @@ result <- xts(result[,2:13], order.by = as.Date(as.character(result[,1]), format
 
 GSoC.CTA <- portfolio.spec(assets = colnames(result))
 
-GSoC.CTA <- add.constraint(portfolio = GSoC.CTA, type = "weight_sum", min_sum = -1, max_sum = 1)
+GSoC.CTA <- add.constraint(portfolio = GSoC.CTA, type = "weight_sum", min_sum = 0, max_sum = 1)
 GSoC.CTA <- add.constraint(portfolio = GSoC.CTA, type = "long_only")
-pspec <- add.constraint(portfolio=pspec,
-                        type="group",
-                        groups=list(c(1, 2, 1), 4),
-                        group_min=c(0.1, 0.15),
-                        group_max=c(0.85, 0.55),
-                        group_labels=c("GroupA", "GroupB"),
-                        group_pos=c(2, 1))
+group_list <- list(group1=c(1, 3, 5),
+                   group2=c(2, 4),
+                   groupA=c(2, 4, 5),
+                   groupB=c(1, 3))
+GSoC.CTA <- add.constraint(portfolio=GSoC.CTA, type="group",
+                        groups=group_list,
+                        group_min=c(0.15, 0.25, 0.2, 0.1),
+                        group_max=c(0.65, 0.55, 0.5, 0.4))
+
 # GSoC.CTA <- add.constraint(portfolio = GSoC.CTA, type = "return", return_target = 0.07)
 
 GSoC.CTA <- add.objective(GSoC.CTA, type = "return", name = "mean")
@@ -33,11 +35,12 @@ test <- function(method) {
   final <- optimize.portfolio(R = result, GSoC.CTA, optimize_method = method, verbos = 0)
   w <- final$weights
   print(paste0("Method: ", method))
+  print(w)
   print(paste0("Sharpe Ratio: ", round((mean(result %*% w) / sd(result %*% w)), 2)))
   print(round((Sys.time() - start),2))
 }
 
-# test("osqp")
+test("osqp")
 
 for (i in methodsList) {
   test(i)
