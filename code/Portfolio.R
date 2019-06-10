4f1ed1a622bcf1a8ce0828b620a2c8354afc18cf
Shawn Feng <xf10@illinois.edu>
2019-06-10 13:36:19 -0500

    Update Portfolio.R

--- code/Portfolio.R
+++ code/Portfolio.R
@@ -7,10 +7,10 @@ rm(list = ls())
 source("~/GitHub/PortfolioAnalytics/R/optimize.portfolio.R")
 
 # Data
-result <- read.csv("~/GitHub/GSoC/data/.combined.csv")
-result <- xts(result[,2:13], order.by = as.Date(as.character(result[,1]), format = "%m/%d/%Y"))
+data <- read.csv("~/GitHub/GSoC/data/.combined.csv")
+data <- xts(data[,2:ncol(data)], order.by = as.Date(as.character(data[,1]), format = "%m/%d/%Y"))
 
-GSoC.CTA <- portfolio.spec(assets = colnames(result))
+GSoC.CTA <- portfolio.spec(assets = colnames(data))
 
 GSoC.CTA <- add.constraint(portfolio = GSoC.CTA, type = "weight_sum", min_sum = 0, max_sum = 1)
 GSoC.CTA <- add.constraint(portfolio = GSoC.CTA, type = "long_only")
@@ -31,17 +31,42 @@ GSoC.CTA <- add.objective(GSoC.CTA, type = "risk", name = "StdDev")
 methodsList <- c("DEoptim", "random", "pso", "GenSA", "osqp")
 
 test <- function(method) {
-  start <- Sys.time()
-  final <- optimize.portfolio(R = result, GSoC.CTA, optimize_method = method, verbos = 0)
-  w <- final$weights
-  print(paste0("Method: ", method))
-  print(w)
-  print(paste0("Sharpe Ratio: ", round((mean(result %*% w) / sd(result %*% w)), 2)))
-  print(round((Sys.time() - start),2))
+  final <- optimize.portfolio(R = data, GSoC.CTA, optimize_method = method, verbos = 0)
+  final$weights
 }
 
 test("osqp")
 
+result <- c()
+
 for (i in methodsList) {
-  test(i)
+  start <- Sys.time()
+  w <- test(i)
+  w[which(w<0)] <- 0
+  result <- rbind(result, c(mean(data %*% w), 
+                            sd(data %*% w), mean(data %*% w)/sd(data %*% w),
+                            as.numeric(Sys.time() - start, units = "secs"),w))
+}
+
+colnames(result) <- c("mean", "sigma", "sharpe ratio", "running time",colnames(data))
+rownames(result) <- methodsList
+
+result <- round(result*100,2)
+result[,4] <- result[,4]/100 
+
+
+sum(result[5,(c(1,3,5)+4)])
+sum(result[5,(c(2,4)+4)])
+sum(result[5,(c(2,4,5)+4)])
+sum(result[5,(c(1,3)+4)])
+
+start <- Sys.time()
+test("random")
+Sys.time() - start
+
+result <- c()
+
+for (i in 1:100) {
+  w <- test("DEoptim")
+  result <- c(result, mean(data %*% w)/sd(data %*% w))
 }
