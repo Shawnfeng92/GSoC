8a01fdf0466853d05dcf4e42a26b82697ce9befe
Shawn Feng <42893396+Shawnfeng92@users.noreply.github.com>
2019-06-14 09:20:51 -0500

    Update CollectingData.r

--- code/CollectingData.r
+++ code/CollectingData.r
@@ -3,26 +3,12 @@ library(foreach)
 library(quantmod)
 library(Quandl)
 library(gtools)
+library(rvest)
 Quandl.api_key("dU-ukkHjcYwUsDqmcvjB")
-rm(list = ls())
 
 cl <- makeCluster(8)
 registerDoParallel(cl)
 
-tickerList <- read.csv(file = "~/Documents/GitHub/GSoC/data/tickers.csv",header = 0)[,1]
-tickerList <- as.character(tickerList)
-
-data <- c()
-for (i in c("MMM", "AAA", "AAPL")) {
-  a <- getSymbols(i, return.class = "xts", warnings = 0, auto.assign = 1)
-  if (class(a) != "try-error") {
-    data <- rbind(data, a)
-  }
-}
 
 
-
-
-
-data <- c()
 stopCluster(cl)
