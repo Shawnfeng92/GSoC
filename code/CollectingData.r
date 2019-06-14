0288a2a5bf58096e3156405b544e28ebad96d482
Shawn Feng <42893396+Shawnfeng92@users.noreply.github.com>
2019-06-13 20:17:04 -0500

    a

--- code/CollectingData.r
+++ code/CollectingData.r
@@ -4,6 +4,8 @@ library(quantmod)
 library(Quandl)
 library(gtools)
 
+rm(list = ls()[ls()!="tickerList"])
+
 Quandl.api_key("dU-ukkHjcYwUsDqmcvjB")
 letter <- chr(65:90)
 tickerList <- letter
@@ -11,57 +13,23 @@ tickerList <- letter
 cl <- makeCluster(8)
 registerDoParallel(cl)
 
-for (i in 1:4) {
-  temp <- c()
-  foreach (j = tickerList) %dopar% {
-    for (k in letter) {
-      temp <- c(temp, paste0(j,k))
-    }
-  }
-  tickerList <- temp
+# for (i in 1:3) {
+#   tickerList <- foreach (j = tickerList, .combine = "c", .packages = "foreach")
+#   %dopar% {
+#     foreach (k = letter, .combine = "c")
+#     %dopar% {
+#       paste0(j,k)
+#     }
+#   }
+# }
+
+foreach (i = tickerList) {
+  
 }
 
 
+
+
+
 data <- c()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 stopCluster(cl)
