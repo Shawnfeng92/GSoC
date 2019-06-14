6773a878602fa158c60bcfc06098099d2021d907
Shawn Feng <42893396+Shawnfeng92@users.noreply.github.com>
2019-06-14 13:05:09 -0500

    Update CollectingData.r

--- code/CollectingData.r
+++ code/CollectingData.r
@@ -5,19 +5,21 @@ library(Quandl)
 library(gtools)
 library(rvest)
 Quandl.api_key("dU-ukkHjcYwUsDqmcvjB")
-
 cl <- makeCluster(8)
 registerDoParallel(cl)
-
 tickerList <- read.csv("~/Documents/GitHub/GSoC/data/tickers.csv")[,2]
-tickerList <- tickerList[2:length(tickerList)]
 tickerList <- as.character(tickerList)
-tickerList <- tickerList[which(!"-" %in% tickerList)]
-
+tickerList <- tickerList[!grepl("-", tickerList)]
+tickerList <- tickerList[!grepl(".", tickerList, fixed = 1)]
+time <- system.time(
 dataset <- foreach(i = tickerList, .combine = "cbind", .packages = "quantmod") %dopar% {
-  getSymbols(i, source="yahoo", auto.assign=FALSE, return.class="xts")[,6]
-}
-
+  temp <- try(getSymbols(i, source="yahoo", auto.assign=FALSE, return.class="xts")[,6])
+  if (class(A) != "try-error") {
+    colnames(temp) <- c(i)
+    temp 
+  } else {
+    return()
+  }
+})
 stopCluster(cl)
-
 rm(list = ls()[which(ls()!="dataset")])
