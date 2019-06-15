354df4b716f8b0d8f4765b4cdd6f2b3a17e7cf59
Shawn Feng <42893396+Shawnfeng92@users.noreply.github.com>
2019-06-15 17:42:56 -0500

    something wrong

--- code/CollectingData.r
+++ code/CollectingData.r
@@ -5,38 +5,40 @@ library(Quandl)
 library(gtools)
 library(rvest)
 
-Quandl.api_key("dU-ukkHjcYwUsDqmcvjB")
-
-cl <- makeCluster(8)
-registerDoParallel(cl)
-
-library('rvest')
-
-time1 <- system.time(
-  stocklist <- foreach(x = chr(65:90), .combine = "c", .packages = "rvest") %dopar%
-  {
-    url <- paste0('http://eoddata.com/stocklist/NYSE/', x, '.htm')
-    webpage <- read_html(url)
-    stockhtml <- html_nodes(webpage,'a')
-    stock <- html_text(stockhtml)
-    stock <- stock[43:length(stock)]
-    stock <- stock[1:(which(stock == "Register")-1)]
-    stock <- stock[which(stock != "")]
-    stock <- stock[!grepl("-", stock)]
-    stock <- stock[!grepl(".", stock, fixed = 1)]
-  }
-)
-
-time2 <- system.time(
-  dataset <- foreach(i = stocklist, .combine = "cbind", .packages = "quantmod") %dopar% {
-    temp <- try(getSymbols(i, source="yahoo", auto.assign=FALSE, return.class="xts")[,6])
-    if (class(temp) != "try-error") {
-      colnames(temp) <- c(i)
-      temp 
-    } else {
-      temp <- NA
+# data collection ----
+collect <- function(){
+  Quandl.api_key("dU-ukkHjcYwUsDqmcvjB")
+  cl <- makeCluster(8)
+  registerDoParallel(cl)
+  time1 <- system.time(
+    stocklist <- foreach(x = chr(65:90), .combine = "c", .packages = "rvest") %dopar%
+    {
+      url <- paste0('http://eoddata.com/stocklist/NYSE/', x, '.htm')
+      webpage <- read_html(url)
+      stockhtml <- html_nodes(webpage,'a')
+      stock <- html_text(stockhtml)
+      stock <- stock[43:length(stock)]
+      stock <- stock[1:(which(stock == "Register")-1)]
+      stock <- stock[which(stock != "")]
+      stock <- stock[!grepl("-", stock)]
+      stock <- stock[!grepl(".", stock, fixed = 1)]
     }
-  }
-)
-
-stopCluster(cl)
+  )
+  write.csv(stocklist,"Documents/GitHub/GSoC/data/tickers.csv")
+  time2 <- system.time(
+    dataset <- foreach(i = stocklist, .combine = "cbind", .packages = "quantmod") %dopar% {
+      temp <- try(getSymbols(i, source="yahoo", auto.assign=FALSE, return.class="xts")[,6])
+      if (class(temp) != "try-error") {
+        colnames(temp) <- c(i)
+        temp 
+      } else {
+        temp <- NA
+      }
+    }
+  )
+  stopCluster(cl)
+  write.zoo(dataset, "Documents/GitHub/GSoC/data/all.csv", sep = ",")
+}
+# choose suitable data ----
+dataset <- read.csv(file = "~/Documents/GitHub/GSoC/data/all.csv")
+dataset <- xts(dataset[,2:ncol(dataset)], as.Date(dataset[,1], "%m/%d/%y"))
