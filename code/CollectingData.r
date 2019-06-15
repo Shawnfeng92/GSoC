484236e3dc3312cc4aa09df9dba5a6f2dc7716b3
Shawn Feng <42893396+Shawnfeng92@users.noreply.github.com>
2019-06-15 09:46:05 -0500

    Update CollectingData.r

--- code/CollectingData.r
+++ code/CollectingData.r
@@ -10,18 +10,33 @@ Quandl.api_key("dU-ukkHjcYwUsDqmcvjB")
 cl <- makeCluster(8)
 registerDoParallel(cl)
 
-tickerList <- read.csv("~/Documents/GitHub/GSoC/data/tickers.csv")[,2]
-tickerList <- as.character(tickerList)
+library('rvest')
+
+time1 <- system.time(
+  stocklist <- foreach(x = chr(65:90), .combine = "c", .packages = "rvest") %dopar%
+  {
+    url <- paste0('http://eoddata.com/stocklist/NYSE/', x, '.htm')
+    webpage <- read_html(url)
+    stockhtml <- html_nodes(webpage,'a')
+    stock <- html_text(stockhtml)
+    stock <- stock[43:length(stock)]
+    stock <- stock[1:(which(stock == "Register")-1)]
+    stock <- stock[which(stock != "")]
+    stock <- stock[!grepl("-", stock)]
+    stock <- stock[!grepl(".", stock, fixed = 1)]
+  }
+)
 
 time <- system.time(
-  dataset <- foreach(i = tickerList, .combine = "cbind", .packages = "quantmod") %dopar% {
-  temp <- try(getSymbols(i, source="yahoo", auto.assign=FALSE, return.class="xts")[,6])
-  if (class(temp) != "try-error") {
-    colnames(temp) <- c(i)
-    temp 
-  } else {
-    temp <- NA
+  dataset <- foreach(i = stocklist, .combine = "cbind", .packages = "quantmod") %dopar% {
+    temp <- try(getSymbols(i, source="yahoo", auto.assign=FALSE, return.class="xts")[,6])
+    if (class(temp) != "try-error") {
+      colnames(temp) <- c(i)
+      temp 
+    } else {
+      temp <- NA
+    }
   }
-})
+)
 
 stopCluster(cl)
