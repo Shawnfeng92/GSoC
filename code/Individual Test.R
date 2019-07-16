08c1d6be36fcad61d7aec248cfb1214476386acb
Shawn Feng <xf10@illinois.edu>
2019-07-16 15:04:05 -0500

    Update Individual Test.R

--- code/Individual Test.R
+++ code/Individual Test.R
@@ -19,13 +19,16 @@ source("~/GitHub/PortfolioAnalytics/R/optimize.portfolio.R")
 data <- read.csv("~/GitHub/GSoC/data/.combined.csv")
 returns <- xts(data[,2:ncol(data)], order.by = as.Date(as.character(data[,1])))
 
+ES <- function(x) {
+  return(mean(x[which(x<quantile(x,0.05))]))
+}
     
 pspec <- portfolio.spec(assets=colnames(returns))
   
-# pspec <- add.objective(pspec, type = "return", name = "mean")
-pspec <- add.objective(pspec, type = "risk", name = "StdDev")
+pspec <- add.objective(pspec, type = "return", name = "mean")
+pspec <- add.objective(pspec, type = "risk", name = "ES")
 
-pspec <- add.constraint(portfolio=pspec, type="weight_sum", min_sum=1, max_sum=1)
+pspec <- add.constraint(portfolio=pspec, type="weight_sum", min_sum=1, max_sum=1.1)
 pspec <- add.constraint(portfolio = pspec, type = "long_only")
 group_list <- list(group1=c(1, 3, 5),
                    group2=c(2, 4),
@@ -36,24 +39,26 @@ pspec <- add.constraint(portfolio=pspec, type="group",
                         group_min=c(0.15, 0.25, 0.2, 0.1),
                         group_max=c(0.65, 0.55, 0.5, 0.4))
 
-osqp.result <- optimize.portfolio(R = returns, portfolio = pspec, optimize_method = "osqp", verbose = FALSE)
+Rglpk.result <- optimize.portfolio(R = returns, portfolio = pspec, optimize_method = "Rglpk", verbose = FALSE)
 
 pso.result <- optimize.portfolio(R = returns, portfolio = pspec, optimize_method = "pso")
 
-osqp.w <- osqp.result$weights
+Rglpk.w <- Rglpk.result$weights
 pso.w <- pso.result$weights
 
-osqp.out <- sd(returns %*% osqp.w)
-pso.out <- sd(returns %*% pso.w)
+Rglpk.out <- mean(Rglpk.w)/ES(returns %*% Rglpk.w)
+pso.out <- mean(pso.w)/ES(returns %*% pso.w)
 
-print(osqp.out)
+print(Rglpk.out)
 print(pso.out)
 
-print(sum(osqp.w))
+print(sum(Rglpk.w))
 print(sum(pso.w))
 
-sapply(group_list, function(x){return(sum(osqp.w[x]))})
+sapply(group_list, function(x){return(sum(Rglpk.w[x]))})
 sapply(group_list, function(x){return(sum(pso.w[x]))})
+
+
 
 
 
