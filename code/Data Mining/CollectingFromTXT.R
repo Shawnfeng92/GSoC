83ad6441147dacc0805908e17d39d57929b20329
Shawn Feng <xf10@illinois.edu>
2019-07-17 11:10:24 -0500

    Improve

--- code/Data Mining/CollectingFromTXT.R
+++ code/Data Mining/CollectingFromTXT.R
@@ -5,24 +5,32 @@
   library(Quandl)
 }
 
-Quandl.api_key("dU-ukkHjcYwUsDqmcvjB")
-cl <- makeCluster(8)
-registerDoSNOW(cl)
-
-tickers <- rbind(
-  read.delim(file = "~/GitHub/GSoC/data/Tickers/AMEX.txt"),
-  read.delim(file = "~/GitHub/GSoC/data/Tickers/NASDAQ.txt"),
-  read.delim(file = "~/GitHub/GSoC/data/Tickers/NYSE.txt"))[,1]
-
-dataset <- foreach(i = tickers, .combine = "cbind", .packages = "quantmod") %dopar% {
-  temp <- try(getSymbols(i, source="yahoo", auto.assign=FALSE, return.class="xts")[,6])
-  if (class(temp) != "try-error") {
-    colnames(temp) <- c(i)
-    temp 
-  } else {
-    temp <- NA
+dataset <- function(filelist = c("AMEX", "NYSE", "NASDAQ")){
+  Quandl.api_key("dU-ukkHjcYwUsDqmcvjB")
+  cl <- makeCluster(8)
+  registerDoSNOW(cl)
+  
+  tickers <- foreach(i = filelist, .combine = "rbind") %dopar% {
+    read.delim(file = paste0("~/GitHub/GSoC/data/Tickers/", i, ".txt"), 
+               stringsAsFactors = FALSE)
+  }[,1]
+  
+  prices <- foreach(i = tickers, .combine = "cbind", .packages = "quantmod") %dopar% {
+    temp <- try(getSymbols(i, source="yahoo", auto.assign=FALSE, return.class="xts")[,6])
+    if (class(temp) != "try-error") {
+      colnames(temp) <- c(i)
+      temp 
+    } else {
+      temp <- NA
+    }
   }
+  stopCluster(cl)
+  
+  
+  
+  
+  
+  
+  
+  returns <- diff(log(prices)) 
 }
-
-stopCluster(cl)
-
