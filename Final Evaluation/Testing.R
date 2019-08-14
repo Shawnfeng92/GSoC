e9a060a65397b841c667f34b9aa78a0910a75083
Shawn Feng <Shawnfeng92@gmail.com>
2019-08-14 08:00:00 -0500

    Update Testing.R

--- Final Evaluation/Testing.R
+++ Final Evaluation/Testing.R
@@ -40,10 +40,81 @@ group_list <- list(group1=c(1, 3, 5),
                    groupB=c(1, 3))
 pspec <- add.constraint(portfolio=pspec, type="group",
                         groups=group_list,
-                        group_min=c(0.15, 0.25, 0.2, 0.1),s
+                        group_min=c(0.15, 0.25, 0.2, 0.1),
                         group_max=c(0.65, 0.55, 0.5, 0.4))
-pRglpk <- optimize.portfolio(returns, pspec, optimize_method = "Rglpk", sil = 1)
+lRglpk <- optimize.portfolio(returns, pspec, optimize_method = "Rglpk", sil = 1)
+lGenSA <- optimize.portfolio(returns, pspec, optimize_method = "GenSA")
+lpso <- optimize.portfolio(returns, pspec, optimize_method = "pso")
+lDEoptim <- optimize.portfolio(returns, pspec, optimize_method = "DEoptim")
+lrandom <- optimize.portfolio(returns, pspec, optimize_method = "random")
+
+for (i in c(lRglpk, lGenSA, lpso, lDEoptim, lrandom)) {
+  print(i)
+}
+
+# comparison test on osqp
+# quadratic programming problems with mean return reward and sigma risk
+
+pspec <- portfolio.spec(assets=colnames(returns))
+
+pspec <- add.objective(pspec, type = "return", name = "mean")
+pspec <- add.objective(pspec, type = "risk", name = "sigma")
+
+pspec <- add.constraint(portfolio=pspec, type="weight_sum", min_sum=1, max_sum=1.1)
+pspec <- add.constraint(portfolio = pspec, type = "long_only")
+group_list <- list(group1=c(1, 3, 5),
+                   group2=c(2, 4),
+                   groupA=c(2, 4, 5),
+                   groupB=c(1, 3))
+pspec <- add.constraint(portfolio=pspec, type="group",
+                        groups=group_list,
+                        group_min=c(0.15, 0.25, 0.2, 0.1),
+                        group_max=c(0.65, 0.55, 0.5, 0.4))
+qosqp <- optimize.portfolio(returns, pspec, optimize_method = "osqp", sil = 1)
+qGenSA <- optimize.portfolio(returns, pspec, optimize_method = "GenSA")
+qpso <- optimize.portfolio(returns, pspec, optimize_method = "pso")
+qDEoptim <- optimize.portfolio(returns, pspec, optimize_method = "DEoptim")
+qrandom <- optimize.portfolio(returns, pspec, optimize_method = "random")
+
+for (i in c(qsqp, qGenSA, qpso, qDEoptim, qrandom)) {
+  print(i)
+}
+
+# comparison test on mco
+# general convex optimize problems with mean return reward and sigma risk
+
+pspec <- portfolio.spec(assets=colnames(returns))
+
+pspec <- add.objective(pspec, type = "return", name = "mean")
+pspec <- add.objective(pspec, type = "risk", name = "sigma")
+
+pspec <- add.constraint(portfolio=pspec, type="weight_sum", min_sum=1, max_sum=1.1)
+pspec <- add.constraint(portfolio = pspec, type = "long_only")
+group_list <- list(group1=c(1, 3, 5),
+                   group2=c(2, 4),
+                   groupA=c(2, 4, 5),
+                   groupB=c(1, 3))
+pspec <- add.constraint(portfolio=pspec, type="group",
+                        groups=group_list,
+                        group_min=c(0.15, 0.25, 0.2, 0.1),
+                        group_max=c(0.65, 0.55, 0.5, 0.4))
+pspec <- add.constraint(portfolio = pspec, type = "position_limit", 6)
+posqp <- optimize.portfolio(returns, pspec, optimize_method = "osqp", sil = 1)
 pGenSA <- optimize.portfolio(returns, pspec, optimize_method = "GenSA")
 ppso <- optimize.portfolio(returns, pspec, optimize_method = "pso")
 pDEoptim <- optimize.portfolio(returns, pspec, optimize_method = "DEoptim")
 prandom <- optimize.portfolio(returns, pspec, optimize_method = "random")
+
+for (i in c(osqp, pGenSA, ppso, pDEoptim, prandom)) {
+  print(i)
+}
+
+
+
+
+
+
+
+
+
+
