d269627fc4e4e74a2942131b36a7bbd9633eae2c
Shawn Feng <Shawnfeng92@gmail.com>
2019-08-17 08:07:23 -0500

    Update Testing.R

--- Final Evaluation/Testing.R
+++ Final Evaluation/Testing.R
@@ -49,12 +49,12 @@ lDEoptim <- optimize.portfolio(returns, pspec, optimize_method = "DEoptim")
 lrandom <- optimize.portfolio(returns, pspec, optimize_method = "random")
 
 # comparison test on osqp
-# quadratic programming problems with mean return reward and sigma risk
+# quadratic programming problems with mean return reward and volatility risk
 
 pspec <- portfolio.spec(assets=colnames(returns))
 
 pspec <- add.objective(pspec, type = "return", name = "mean")
-pspec <- add.objective(pspec, type = "risk", name = "sigma")
+pspec <- add.objective(pspec, type = "risk", name = "StdDev")
 
 pspec <- add.constraint(portfolio=pspec, type="weight_sum", min_sum=1, max_sum=1.1)
 pspec <- add.constraint(portfolio = pspec, type = "long_only")
@@ -73,12 +73,12 @@ qDEoptim <- optimize.portfolio(returns, pspec, optimize_method = "DEoptim")
 qrandom <- optimize.portfolio(returns, pspec, optimize_method = "random")
 
 # comparison test on mco
-# general convex optimize problems with mean return reward and sigma risk
+# general convex optimize problems with mean return reward and volatility risk
 
 pspec <- portfolio.spec(assets=colnames(returns))
 
 pspec <- add.objective(pspec, type = "return", name = "mean")
-pspec <- add.objective(pspec, type = "risk", name = "sigma")
+pspec <- add.objective(pspec, type = "risk", name = "StdDev")
 
 pspec <- add.constraint(portfolio=pspec, type="weight_sum", min_sum=1, max_sum=1.1)
 pspec <- add.constraint(portfolio = pspec, type = "long_only")
