6fb677c8e5ef0806439151ee7131c50b2aa3bea6
Shawn Feng <Shawnfeng92@gmail.com>
2019-08-26 08:26:02 -0500

    Update Testing.R

--- Final Evaluation/Testing.R
+++ Final Evaluation/Testing.R
@@ -47,7 +47,7 @@ lGenSA <- optimize.portfolio(returns, pspec, optimize_method = "GenSA")
 lpso <- optimize.portfolio(returns, pspec, optimize_method = "pso")
 lDEoptim <- optimize.portfolio(returns, pspec, optimize_method = "DEoptim")
 lrandom <- optimize.portfolio(returns, pspec, optimize_method = "random")
-
+lRglpk
 # comparison test on osqp
 # quadratic programming problems with mean return reward and volatility risk
 
