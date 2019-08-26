edc19492ea7acd63e9d7a0865c8f37e28aebf2db
Shawn Feng <Shawnfeng92@gmail.com>
2019-08-26 08:16:28 -0500

    Update Testing.R

--- Final Evaluation/Testing.R
+++ Final Evaluation/Testing.R
@@ -66,6 +66,7 @@ pspec <- add.constraint(portfolio=pspec, type="group",
                         groups=group_list,
                         group_min=c(0.15, 0.25, 0.2, 0.1),
                         group_max=c(0.65, 0.55, 0.5, 0.4))
+
 qosqp <- optimize.portfolio(returns, pspec, optimize_method = "osqp", sil = 1)
 qGenSA <- optimize.portfolio(returns, pspec, optimize_method = "GenSA")
 qpso <- optimize.portfolio(returns, pspec, optimize_method = "pso")
@@ -91,7 +92,7 @@ pspec <- add.constraint(portfolio=pspec, type="group",
                         group_min=c(0.15, 0.25, 0.2, 0.1),
                         group_max=c(0.65, 0.55, 0.5, 0.4))
 pspec <- add.constraint(portfolio = pspec, type = "position_limit", max_pos = 6)
-posqp <- optimize.portfolio(returns, pspec, optimize_method = "osqp", sil = 1)
+pmco <- optimize.portfolio(returns, pspec, optimize_method = "mco", sil = 1)
 pGenSA <- optimize.portfolio(returns, pspec, optimize_method = "GenSA")
 ppso <- optimize.portfolio(returns, pspec, optimize_method = "pso")
 pDEoptim <- optimize.portfolio(returns, pspec, optimize_method = "DEoptim")
