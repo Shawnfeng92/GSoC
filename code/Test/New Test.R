53f453dbe1646df048b44a704bdde6bde581b77b
Shawnfeng92 <Shawnfeng92@gmail.com>
2020-01-13 16:19:11 -0600

    Update New Test.R

--- code/Test/New Test.R
+++ code/Test/New Test.R
@@ -1,53 +1,61 @@
+# ====
+
 library(PortfolioAnalytics)
 library(osqp)
 library(Rglpk)
 library(mco)
 
-source(file = "code/Test/optimize.portfolio.R")
+source(file = "GitHub/GSoC/code/Test/optimize.portfolio.R")
 
 data(edhec)
 returns <- edhec
-fund.names <- colnames(returns)
 
-pspec <- portfolio.spec(assets = fund.names)
-
-# Add the full investment constraint that specifies the weights must sum to 1.
-pspec <- add.constraint(
-  portfolio = pspec, type = "weight_sum",
-  min_sum = 1, max_sum = 1
+rm(
+  edhec,
+  optimize.portfolio.rebalancing_v1,
+  optimize.portfolio.parallel,
+  optimize.portfolio.rebalancing,
+  optimize.portfolio_v1
 )
 
-# min and max can also be specified per asset
+# ====
+pspec <- portfolio.spec(assets = colnames(returns))
+pspec <- add.constraint(
+  portfolio = pspec, type = "weight_sum",
+  min_sum = -1, max_sum = 1
+)
+
 pspec <- add.constraint(
   portfolio = pspec,
   type = "box",
-  min = c(0.05, 0, 0.08, 0.1, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-  max = c(0.4, 0.3, 0.7, 0.55, 1, 1, 1, 1, 1, 1, 1, 1, 1)
+  min = rep(-0.5, 13),
+  max = 1:13/10
 )
 
-# Add group constraints
 pspec <- add.constraint(
   portfolio = pspec,
   type = "group",
-  groups = list(c(1, 2), 4),
-  group_min = c(0.1, 0.15),
-  group_max = c(0.85, 0.55),
-  group_labels = c("GroupA", "GroupB")
+  groups = list(
+    c(1, 2, 3),
+    c(2, 3, 4)
+  ),
+  group_min = c(0, 0.1),
+  group_max = c(0.5, 0.6)
 )
 
-# Add position limit constraint such that we have a maximum number
-# of three assets with non-zero weights.
-# pspec <- add.constraint(portfolio = pspec, type = "position_limit", max_pos = 3)
+pspec <- add.constraint(
+  portfolio = pspec,
+  type = "return",
+  return_target = 0.005
+)
 
-# Add diversification constraint
-pspec <- add.constraint(portfolio = pspec, type = "diversification", div_target = 0.7)
-
-# Add target mean return constraint
-pspec <- add.constraint(portfolio = pspec, type = "return", return_target = 0.007)
-
-# Add objective
 pspec <- add.objective(pspec, type = "return", name = "mean")
 pspec <- add.objective(pspec, type = "risk", name = "StdDev")
 
-result <- optimize.portfolio(R = returns, portfolio = pspec, optimize_method = "pso",
-                             trace = FALSE, message = FALSE)
+osqp.result <- optimize.portfolio(
+  R = returns, portfolio = pspec,
+  optimize_method = "osqp", trace = FALSE,
+  message = FALSE
+)
+
+PortfolioAnalytics:::check_constraints(osqp.result$x, pspec)
